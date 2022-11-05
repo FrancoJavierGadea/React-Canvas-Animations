81f85b49b04f8e43a79229e59f2857d98bd0acc9
1812 <francogadea1812@gmail.com>
2022-11-05 01:45:38 -0300

    Agregando React-Cropper para recortar las imagenes cargadas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 
 //? Bootstrap and Bootstrap icons
@@ -11,30 +11,38 @@ import styled from "styled-components";
 
 import blackhole from "./assets/images/blackhole.jpg";
 import { getBase64 } from './helpers/ImageLoader';
+import ModalCropper from './components/ModalCropper';
 
+
+import bg from "./assets/images/cropper-bg.png";
 
 const StyledContainer = styled.div`
 
   --grid-bg-size: 15px;
-  --grid-bg-color: #ffffff50;
+  --grid-bg-color: #ffffff20;
   
   height: 80vh;
+  max-height: 700px;
 
   background-size: var(--grid-bg-size, 40px) var(--grid-bg-size, 40px);    
   background-image: repeating-linear-gradient(0deg, var(--grid-bg-color, #fff), var(--grid-bg-color, #fff) 1px, transparent 1px, transparent var(--grid-bg-size, 40px)), repeating-linear-gradient(-90deg, var(--grid-bg-color, #fff), var(--grid-bg-color, #fff) 1px, transparent 1px, transparent var(--grid-bg-size, 40px));
+
+  background-image: url(${bg});
 
   padding: 0;
 
   img {
     width: 100%;
     height: 100%;
-    object-fit: scale-down;
+    object-fit: contain;
   }
 
 `;
 
 
 function App() {
+
+  const modal = useRef();
 
   const [image, setImage] = useState(blackhole);
 
@@ -46,7 +54,11 @@ function App() {
 
     img.then((value) => {
 
-      setImage(value.base64);
+      modal.current.setFileType(file.type);
+
+      modal.current.setCropperImage(value.base64);
+
+      modal.current.show();
     })
     .catch((error) => {
 
@@ -57,21 +69,31 @@ function App() {
   return (
     <div className="App bg-dark p-2 vh-100">
       
-      <Container className="p-0 border border-secondary rounded">
+      <Container className="p-0 rounded">
         <StyledContainer>
-          <img src={image}></img>
+          <img src={image} alt=""></img>
         </StyledContainer>
       </Container>
 
 
-      <Container className="bg-light my-2 p-2 rounded d-flex justify-content-around">
+      <Container className="bg-light my-2 p-2 rounded d-flex justify-content-evenly">
 
         <label className="btn btn-primary">
           <input className="d-none" type="file" accept="image/*" id="img-input" onChange={loadImage}/>
+          <i className="bi bi-upload mx-2"></i>
           Cargar Imagen
         </label>
 
+        <Button variant="secondary" onClick={() => modal.current.show()}>Update Image</Button>
+
+        <a className="btn btn-success" href={image} target="_blank" rel="noreferrer" download="image">
+          <i className="bi bi-download mx-2"></i>
+          Download
+        </a>
+
       </Container>
+
+      <ModalCropper ref={modal} setImage={setImage}></ModalCropper>
 
     </div>
   );
---
+++ src/components/ModalCropper.jsx
@@ -0,0 +1,130 @@
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
+import { Button, Col, Container, Modal, Row } from 'react-bootstrap';
+import { Cropper } from 'react-cropper';
+
+
+import "cropperjs/dist/cropper.css";
+import blackhole from "../assets/images/blackhole.jpg";
+import { useRef } from 'react';
+import styled from 'styled-components';
+
+import bg from "../assets/images/cropper-bg.png";
+
+
+const StyledPreview = styled.div`
+
+    width: 100%;
+    height: auto;
+
+    overflow: hidden;
+
+    background-image: url(${bg});
+
+    & > div {
+        overflow: hidden;
+        width: 100%;
+        height: 400px;
+    }
+`;
+
+
+function ModalCropper({setImage}, ref){
+
+    //* Modal
+    const [visible, setVisible] = useState(false);
+
+    const show = () => setVisible(true);
+    
+    const hide = () => setVisible(false);
+    
+
+    //* Cropper
+    const cropperRef = useRef(null);
+
+    const [cropperImage, setCropperImage] = useState(blackhole);
+    
+    const [fileType, setFileType] = useState('image/jpg');
+
+    const [details, setDetails] = useState({});
+    
+    const save = () => {
+
+        const cropper = cropperRef.current.cropper;
+
+        const cropImage = cropper.getCroppedCanvas({
+            minWidth: 256,
+            minHeight: 256,
+            maxWidth: 4096,
+            maxHeight: 4096,
+            fillColor: 'transparent',
+            imageSmoothingEnabled: true,
+            imageSmoothingQuality: 'high'
+        }).toDataURL(fileType);
+
+        //console.log(cropImage);
+
+        setImage(cropImage);
+
+        hide();
+    };
+
+    const onCrop = (e) => {
+
+        setDetails({...e.detail});
+    }
+
+
+    //Exponer metodos a los Componentes Padres
+    useImperativeHandle(ref, () => ({show, hide, setCropperImage, setFileType}));
+
+
+    return (<div className="ModalCropper">
+
+        <Modal show={visible} onHide={hide} backdrop="static" keyboard={false} size="xl">
+
+            <Modal.Header closeButton>
+                <Modal.Title>Cropper</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+
+                <Container>
+                    <Row>
+                        <Col lg={7} xs={12}>
+                            <Cropper src={cropperImage} ref={cropperRef} style={{height: 500, width: "100%"}} guides={false} viewMode={1} preview={'.img-preview'} crop={onCrop}/>
+                        </Col>
+
+                        <Col lg={5} xs={12}>
+                            <h3 className="text-center">Preview</h3>
+
+                            <StyledPreview>
+                                <div className="img-preview"></div>
+                            </StyledPreview>
+
+                            <div className="p-2 d-flex justify-content-evenly">
+                                <ul class="list-group list-group-horizontal-sm">
+                                    <li class="list-group-item list-group-item-info">width</li>
+                                    <li class="list-group-item">{Math.floor(details.width)} px</li>
+                                </ul>
+     
+                                <ul class="list-group list-group-horizontal-sm">
+                                    <li class="list-group-item list-group-item-info">height</li>
+                                    <li class="list-group-item">{Math.floor(details.height)} px</li>
+                                </ul>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+
+            </Modal.Body>
+
+            <Modal.Footer>
+                <Button variant="secondary" onClick={hide}>Close</Button>
+                <Button variant="primary" onClick={save}>Save</Button>
+            </Modal.Footer>
+        </Modal>
+ 
+    </div>);
+};
+
+export default forwardRef(ModalCropper);
